8633fa9351df0ea14294b8b3cab5d3fa95d1e8b8
Felix <felix.rustemeyer@gmail.com>
2018-02-23 22:36:57 +0100

    new site

---
+++ index.js
@@ -0,0 +1,9 @@
+var count = localStorage.getItem("count");
+$("#count").text(count);
+
+$("#clickBtn").click(function(){
+    count++
+    $("#count").text(count);
+    localStorage.setItem("count", count);
+});
+
